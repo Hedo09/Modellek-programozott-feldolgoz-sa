2db61e365c517cde49d4bf4e88f740ecdcb06339
Hedo09 <49043953+Hedo09@users.noreply.github.com>
2021-03-11 17:26:07 +0100

    Add files via upload
    Feladat 3

--- REMO_WS/hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
+++ REMO_WS/hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
@@ -1,31 +1,48 @@
 package hu.bme.mit.yakindu.analysis.workhere;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
-// import hu.bme.mit.yakindu.analysis.RuntimeService;
-// import hu.bme.mit.yakindu.analysis.TimerService;
+import hu.bme.mit.yakindu.analysis.RuntimeService;
+import hu.bme.mit.yakindu.analysis.TimerService;
 import hu.bme.mit.yakindu.analysis.example.ExampleStatemachine;
 import hu.bme.mit.yakindu.analysis.example.IExampleStatemachine;
+
 
 
 
 public class RunStatechart {
 	
 	public static void main(String[] args) throws IOException {
-//		ExampleStatemachine s = new ExampleStatemachine();
-//		s.setTimer(new TimerService());
-//		RuntimeService.getInstance().registerStatemachine(s, 200);
-//		s.init();
-//		s.enter();
-//		s.runCycle();
-//		print(s);
-//		s.raiseStart();
-//		s.runCycle();
-//		System.in.read();
-//		s.raiseWhite();
-//		s.runCycle();
-//		print(s);
-//		System.exit(0);
+		ExampleStatemachine s = new ExampleStatemachine();
+		s.setTimer(new TimerService());
+		RuntimeService.getInstance().registerStatemachine(s, 200);
+		s.init();
+		s.enter();
+		s.runCycle();
+		BufferedReader reader = new BufferedReader( new InputStreamReader(System.in));
+		while(true) {
+			String name = reader.readLine();
+			if(name.equals("start")) {
+				s.raiseStart();
+				s.runCycle();
+			}
+			else if(name.equals("black")) {
+				s.raiseBlack();
+				s.runCycle();
+			}
+			else if(name.equals("white")) {
+				s.raiseWhite();
+				s.runCycle();
+			}
+			else if(name.equals("exit")) {
+				print(s);
+				System.exit(0);
+			}
+			print(s);
+		}
+		
 	}
 
 	public static void print(IExampleStatemachine s) {
